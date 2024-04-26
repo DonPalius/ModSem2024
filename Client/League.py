07f27b655c07190e516bb7d86395edb5e9220937
DonPalius <palettoandrea@gmail.com>
2024-04-26 11:12:50 +0200

    For loop

--- Client/League.py
+++ Client/League.py
@@ -4,94 +4,52 @@ from Query import LoLQueries
 endpoint_url = "http://localhost:7200/repositories/LOL" 
 lol_queries = LoLQueries(endpoint_url)  
 
-
-
 st.title('League of Legends Knowledge Base')
 st.write("League of Legends Ontology for Semantic Web Exam")
 
 selected_option = st.sidebar.selectbox("Select an action:", [
-  "Get Assassin Champions",
-  "Get List of Players",
-  "Visualize Abilities (Champion Name Required)",
-  "Get Tank Count",
-  "Get Domination Runes",
-  "Get Loadout for Player (Match Name Required)",
-  "Get Attack Stat Objects",
-  "Get Ability Power Scaling Champions",
-  "Get Players in Match (Match Name Required)",
-  "Get Rune Pages Used by Player",
-  "Get Available Game Modes",
-  "Get Tournaments Participated by Player",
-  "Get Teams Played by Player"
+ "Get Assassin Champions",
+ "Get List of Players",
+ "Visualize Abilities (Champion Name Required)",
+ "Get Tank Count",
+ "Get Domination Runes",
+ "Get Loadout for Player (Match Name Required)",
+ "Get Attack Stat Objects",
+ "Get Ability Power Scaling Champions",
+ "Get Players in Match (Match Name Required)",
+ "Get Rune Pages Used by Player",
+ "Get Available Game Modes",
+ "Get Tournaments Participated by Player",
+ "Get Teams Played by Player"
 ])
 
-if selected_option == "Get Assassin Champions":
-  champions = lol_queries.get_assassin_champions()
-  st.write("Assassin Champions:")
-  st.write(champions)
-elif selected_option == "Get List of Players":
-  players = lol_queries.get_players_list()
-  st.write("Players:")
-  st.write(players)
-elif selected_option == "Visualize Abilities (Champion Name Required)":
-  champion_name = st.text_input("Enter Champion Name:")
-  if champion_name:
-    abilities = lol_queries.get_champion_abilities(champion_name)
-    st.write(f"{champion_name}'s Abilities:")
-    st.write(abilities)
-  else:
-    st.write("Please enter a champion name.")
-elif selected_option == "Get Tank Count":
-  tank_count = lol_queries.get_tank_champions_count()
-  st.write("Number of Tank Champions:")
-  st.write(tank_count)
-elif selected_option == "Get Domination Runes":
-  domination_runes = lol_queries.get_dominance_runes()
-  st.write("Domination Runes:")
-  st.write(domination_runes)
-elif selected_option == "Get Loadout for Player (Match Name Required)":
-  player_name = st.text_input("Enter Player Name:")
-  match_name = st.text_input("Enter Match Name:")
-  if player_name and match_name:
-    loadout = lol_queries.get_loadout_for_player_in_match(player_name, match_name)
-    st.write(f"{player_name}'s Loadout in {match_name}:")
-    st.write(loadout)
-  else:
-    st.write("Please enter both player name and match name.")
-elif selected_option == "Get Attack Stat Objects":
-  attack_objects = lol_queries.get_attack_stat_objects()
-  st.write("Attack Stat Objects:")
-  st.write(attack_objects)
-elif selected_option == "Get Ability Power Scaling Champions":
-  ap_champions = lol_queries.get_ability_power_scaling_champions()
-  st.write("Ability Power Scaling Champions:")
-  st.write(ap_champions)
-elif selected_option == "Get Players in Match (Match Name Required)":
-  match_name = st.text_input("Enter Match Name:")
-  if match_name:
-    players = lol_queries.get_players_in_match(match_name)
-    st.write(f"Players in Match {match_name}:")
-    st.write(players)
-  else:
-    st.write("Please enter a match name.")
-elif selected_option == "Get Rune Pages Used by Player":
-  player_name = st.text_input("Enter Player Name:")
-  if player_name:
-    rune_pages = lol_queries.get_rune_pages_used_by_player(player_name)
-    st.write(f"{player_name}'s Rune Pages:")
-    st.write(rune_pages)
-  else:
-    st.write("Please enter a player name.")
-elif selected_option == "Get Available Game Modes":
-  game_modes = lol_queries.get_game_modes_available()
-  st.write("Available Game Modes:")
-  st.write(game_modes)
-elif selected_option == "Get Tournaments Participated by Player":
-  player_name = st.text_input("Enter Player Name:")
-  if player_name:
-    tournaments = lol_queries.get_tournaments_participated
+# Define a dictionary mapping options to functions
+option_to_function = {
+ "Get Assassin Champions": lol_queries.get_assassin_champions,
+ "Get List of Players": lol_queries.get_players_list,
+ "Visualize Abilities (Champion Name Required)": lambda: lol_queries.get_champion_abilities(st.text_input("Enter Champion Name:")),
+ "Get Tank Count": lol_queries.get_tank_champions_count,
+ "Get Domination Runes": lol_queries.get_dominance_runes,
+ "Get Loadout for Player (Match Name Required)": lambda: lol_queries.get_loadout_for_player_in_match(st.text_input("Enter Player Name:"), st.text_input("Enter Match Name:")),
+ "Get Attack Stat Objects": lol_queries.get_attack_stat_objects,
+ "Get Ability Power Scaling Champions": lol_queries.get_ability_power_scaling_champions,
+ "Get Players in Match (Match Name Required)": lambda: lol_queries.get_players_in_match(st.text_input("Enter Match Name:")),
+ "Get Rune Pages Used by Player": lambda: lol_queries.get_rune_pages_used_by_player(st.text_input("Enter Player Name:")),
+ "Get Available Game Modes": lol_queries.get_game_modes_available,
+ "Get Tournaments Participated by Player": lambda: lol_queries.get_tournaments_participated(st.text_input("Enter Player Name:")),
+ "Get Teams Played by Player": lambda: lol_queries.get_teams_played_by_player(st.text_input("Enter Player Name:"))
+}
 
-
+# Execute the function corresponding to the selected option
+if selected_option in option_to_function:
+    result = option_to_function[selected_option]()
+    if isinstance(result, tuple):
+        st.write(f"{result[0]}:")
+        st.write(result[1])
+    else:
+        st.write(result)
+else:
+    st.write("Please select an option.")
 
 footer = """
 <style>
