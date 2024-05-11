5c4b3329d65ae9f1bafc8812f686cff724c9fc2c
Palius <palettoandrea@gmail.com>
2024-05-11 11:10:58 +0200

    Commented Query

--- Client/League.py
+++ Client/League.py
@@ -1,23 +1,24 @@
+# Import necessary libraries
 import streamlit as st
-from Query import LoLQueries  
+from Query import LoLQueries  # Custom module for handling queries
 import pandas as pd
 
+# Define the endpoint URL for the League of Legends ontology
 endpoint_url = "http://localhost:7200/repositories/LOL" 
-lol_queries = LoLQueries(endpoint_url)  
+lol_queries = LoLQueries(endpoint_url)  # Initialize the query handler with the endpoint
 
+# Set the title of the Streamlit app
 st.title('League of Legends Ontology')
 
-
-
-
-
+# Function to convert query results into a pandas DataFrame and display it
 def get_table_from_results(results):
-    # Define the headers
+    # Define the headers for the DataFrame
     headers = ["Label", "Comment"]
     
     # Initialize an empty DataFrame with the specified headers
     df = pd.DataFrame(columns=headers)
 
+    # Iterate through each result and extract the label and comment
     for binding in results["results"]["bindings"]:
         label = binding.get("label", {}).get("value") 
         comment = binding.get("comment", {}).get("value")
@@ -25,22 +26,20 @@ def get_table_from_results(results):
         # Append each row to the DataFrame
         df = df.append({"Label": label, "Comment": comment}, ignore_index=True)
 
-    # Display the DataFrame
+    # Display the DataFrame in the Streamlit app
     st.dataframe(df, use_container_width=True)
 
-
-
-
-# Define tabs
+# Define tabs for different types of information
 tabs = st.tabs(["Champion Information", "Player Information", "Game Information"])
 
+# Function to execute a query based on the selected option
 def run_query(selected_option):
-  # Execute the function corresponding to the selected option
-  if selected_option in option_to_function:
+    # Execute the function corresponding to the selected option
+    if selected_option in option_to_function:
         result = option_to_function[selected_option]()
         get_table_from_results(result)
-  else:
-      st.write("Please select an option.")
+    else:
+        st.write("Please select an option.")
 
 # Define a dictionary mapping options to functions
 option_to_function = {
@@ -58,7 +57,7 @@ option_to_function = {
     "Get Teams Played by Rekkles":  lol_queries.get_teams_played_by_player
 }
 
-# Run queries based on tabs
+# Run queries based on the selected tab
 with tabs[0]:  # Champion Information Tab
     selected_option = st.selectbox("Select an action:", [
         "Get Assassin Champions",
@@ -86,6 +85,7 @@ with tabs[2]:  # Game Information Tab
     ])
     run_query(selected_option)
 
+# Define the footer HTML and CSS
 footer = """
 <style>
 .footer {
@@ -101,7 +101,7 @@ footer = """
 <div class="footer">
     <p>© 2024 Progetto di Modellazione Concettuale per il Web Semantico. All rights reserved.</p>
 </div>
-
 """
 
+# Display the footer in the Streamlit app
 st.markdown(footer, unsafe_allow_html=True)
--- Client/Query.py
+++ Client/Query.py
@@ -1,234 +1,239 @@
+# Import SPARQLWrapper for executing SPARQL queries and JSON for handling JSON data
 from SPARQLWrapper import SPARQLWrapper, JSON
 
+# Define a class for handling League of Legends (LoL) queries
 class LoLQueries:
     def __init__(self, endpoint_url):
+        # Initialize SPARQLWrapper with the endpoint URL and set the return format to JSON
         self.sparql = SPARQLWrapper(endpoint_url)
         self.sparql.setReturnFormat(JSON)
 
+    # Method to execute a SPARQL query and return the results
     def execute_query(self, query):
         self.sparql.setQuery(query)
         results = self.sparql.query().convert()
         return results
 
+    # Method to get assassin champions
     def get_assassin_champions(self):
         query = """
-        PREFIX LoL: <http://www.semanticweb.org/drunkfazor/ontologies/2024/2/LoL#>
+        PREFIX LoL: <http://www.semanticweb.org/gfazor/ontologies/LOL#>
         PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
         PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-        SELECT ?label ?comment
+        SELECT?label?comment
         WHERE {
-            ?x rdf:type LoL:Assassin.
-            OPTIONAL { ?x rdfs:label ?label }
-            OPTIONAL { ?x rdfs:comment ?comment }
+          ?x rdf:type LoL:Assassin.
+            OPTIONAL {?x rdfs:label?label }
+            OPTIONAL {?x rdfs:comment?comment }
         }
-
         """
         return self.execute_query(query)
 
+    # Method to get a list of players that played a match
     def get_players_list(self):
         query = """
         PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-        PREFIX LoL: <http://www.semanticweb.org/drunkfazor/ontologies/2024/2/LoL#>
+        PREFIX LoL: <http://www.semanticweb.org/gfazor/ontologies/LOL#>
         PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-        select ?y ?label ?comment
-        where {
-            ?x rdf:type LoL:Match_role;
-            LoL:playerParticipating ?y.
-            OPTIONAL { ?x rdfs:label ?label }
-            OPTIONAL { ?x rdfs:comment ?comment }
+        SELECT?y?label?comment
+        WHERE {
+          ?x rdf:type LoL:Match_role;
+            LoL:playerParticipating?y.
+            OPTIONAL {?x rdfs:label?label }
+            OPTIONAL {?x rdfs:comment?comment }
         }
-        GROUPBY ?y ?label ?comment
-
+        GROUP BY?y?label?comment
         """
         return self.execute_query(query)
 
+    # Method to get the abilities of a specific champion (e.g., Zed)
     def get_champion_abilities(self):
-        ## di zed 
         query = """
         PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-        PREFIX LoL: <http://www.semanticweb.org/drunkfazor/ontologies/2024/2/LoL#>
+        PREFIX LoL: <http://www.semanticweb.org/gfazor/ontologies/LOL#>
         PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-        select ?x ?label ?comment
-        where {
-            ?x rdf:type LoL:Abilità;
-            LoL:ability_of ?y.
-            VALUES ?y {LoL:Zed}.
-            OPTIONAL { ?x rdfs:label ?label }
-            OPTIONAL { ?x rdfs:comment ?comment }
+        SELECT?x?label?comment
+        WHERE {
+          ?x rdf:type LoL:Abilità;
+            LoL:ability_of?y.
+            VALUES?y {LoL:Zed}.
+            OPTIONAL {?x rdfs:label?label }
+            OPTIONAL {?x rdfs:comment?comment }
         }
         """
         return self.execute_query(query)
 
+    # Method to get the count of tank champions
     def get_tank_champions_count(self):
         query = """
         PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-        PREFIX LoL: <http://www.semanticweb.org/drunkfazor/ontologies/2024/2/LoL#>
+        PREFIX LoL: <http://www.semanticweb.org/gfazor/ontologies/LOL#>
         PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-        select (count(?y) as ?count)
-        where {
-            ?x rdf:type ?y.
-            VALUES ?y {LoL:Tank}.
+        SELECT (COUNT(?y) AS?count)
+        WHERE {
+          ?x rdf:type?y.
+            VALUES?y {LoL:Tank}.
         }
-
         """
         return self.execute_query(query)
 
+    # Method to get runes in the Domination path
     def get_dominance_runes(self):
         query = """
         PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-        PREFIX LoL: <http://www.semanticweb.org/drunkfazor/ontologies/2024/2/LoL#>
+        PREFIX LoL: <http://www.semanticweb.org/gfazor/ontologies/LOL#>
         PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-        select  ?label ?comment
-        where{
-            ?x rdf:type LoL:Rune;
+        SELECT?label?comment
+        WHERE {
+          ?x rdf:type LoL:Rune;
             LoL:in_path LoL:Domination.
-            OPTIONAL { ?x rdfs:label ?label }
-            OPTIONAL { ?x rdfs:comment ?comment }
-
+            OPTIONAL {?x rdfs:label?label }
+            OPTIONAL {?x rdfs:comment?comment }
         }
         """
         return self.execute_query(query)
 
+    # Method to get the loadout of faker in a match
     def get_loadout_for_player_in_match(self, player_name):
         query = f"""
         PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-        PREFIX LoL: <http://www.semanticweb.org/drunkfazor/ontologies/2024/2/LoL#>
-        SELECT ?loadout WHERE {{
-            ?player rdf:type LoL:Player;
-                    LoL:hasLoadout ?loadout.
-            ?match_role rdf:type LoL:Match_role;
-                        LoL:playerParticipating ?player.
+        PREFIX LoL: <http://www.semanticweb.org/gfazor/ontologies/LOL#>
+        SELECT?loadout
+        WHERE {{
+          ?player rdf:type LoL:Player;
+                    LoL:hasLoadout?loadout.
+          ?match_role rdf:type LoL:Match_role;
+                        LoL:playerParticipating?player.
             FILTER (?player = LoL:{player_name})
         }}
         """
         return self.execute_query(query)
 
+    # Method to get attack stat objects
     def get_attack_stat_objects(self):
         query = """
         PREFIX : <http://www.semanticweb.org/drunkfazor/ontologies/2024/2/LoL/>
         PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-        PREFIX LoL: <http://www.semanticweb.org/drunkfazor/ontologies/2024/2/LoL#>
+        PREFIX LoL: <http://www.semanticweb.org/gfazor/ontologies/LOL#>
         PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-        select  ?label ?comment
-        where {
-            ?x rdf:type LoL:Oggetto;
-            LoL:has_stat ?y.
-            ?y rdf:type LoL:Attack.
-            OPTIONAL { ?x rdfs:label ?label }
-            OPTIONAL { ?x rdfs:comment ?comment }
-
+        SELECT?label?comment
+        WHERE {
+          ?x rdf:type LoL:Oggetto;
+            LoL:has_stat?y.
+          ?y rdf:type LoL:Attack.
+            OPTIONAL {?x rdfs:label?label }
+            OPTIONAL {?x rdfs:comment?comment }
         }
         """
         return self.execute_query(query)
 
+    # Method to get champions that scale with ability power
     def get_ability_power_scaling_champions(self):
         query = """
         PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-        PREFIX LoL: <http://www.semanticweb.org/drunkfazor/ontologies/2024/2/LoL#>
+        PREFIX LoL: <http://www.semanticweb.org/gfazor/ontologies/LOL#>
         PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-        select ?label ?comment
-        where {
-            ?x rdf:type LoL:Campione;
-            LoL:scales_with_stat ?y.
-            ?y rdf:type ?z.
-            ?z rdfs:subClassOf LoL:Magic.
-            OPTIONAL { ?x rdfs:label ?label }
-            OPTIONAL { ?x rdfs:comment ?comment }
+        SELECT?label?comment
+        WHERE {
+          ?x rdf:type LoL:Campione;
+            LoL:scales_with_stat?y.
+          ?y rdf:type?z.
+          ?z rdfs:subClassOf LoL:Magic.
+            OPTIONAL {?x rdfs:label?label }
+            OPTIONAL {?x rdfs:comment?comment }
         }
         """
         return self.execute_query(query)
 
+    # Method to get players in a match
     def get_players_in_match(self):
         query = """
         PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-        PREFIX LoL: <http://www.semanticweb.org/drunkfazor/ontologies/2024/2/LoL#>
+        PREFIX LoL: <http://www.semanticweb.org/gfazor/ontologies/LOL#>
         PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-        select ?y ?label ?comment
-        where {
-            ?x rdf:type LoL:Match_role;
-            LoL:playerParticipating ?y.
-            OPTIONAL { ?x rdfs:label ?label }
-            OPTIONAL { ?x rdfs:comment ?comment }
+        SELECT?y?label?comment
+        WHERE {
+          ?x rdf:type LoL:Match_role;
+            LoL:playerParticipating?y.
+            OPTIONAL {?x rdfs:label?label }
+            OPTIONAL {?x rdfs:comment?comment }
         }
-        GROUPBY ?y ?label ?comment
+        GROUP BY?y?label?comment
         """
         return self.execute_query(query)
 
+    # Method to get rune pages used by a specific player
     def get_rune_pages_used_by_player(self):
         query = """
         PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-        PREFIX LoL: <http://www.semanticweb.org/drunkfazor/ontologies/2024/2/LoL#>
-        select ?label ?comment
-        where {
-            ?z rdf:type LoL:Rune_Page.
-            ?y rdf:type LoL:Loadout.
-            ?x rdf:type LoL:Match_role;
+        PREFIX LoL: <http://www.semanticweb.org/gfazor/ontologies/LOL#>
+        SELECT?label?comment
+        WHERE {
+          ?z rdf:type LoL:Rune_Page.
+          ?y rdf:type LoL:Loadout.
+          ?x rdf:type LoL:Match_role;
             LoL:playerParticipating LoL:Faker.
-            OPTIONAL { ?z rdfs:label ?label }
-            OPTIONAL { ?z rdfs:comment ?comment }
-            
+            OPTIONAL {?z rdfs:label?label }
+            OPTIONAL {?z rdfs:comment?comment }
         }
-        GROUPBY  ?label ?comment
-
+        GROUP BY?label?comment
         """
         return self.execute_query(query)
 
+    # Method to get available game modes
     def get_game_modes_available(self):
         query = """
         PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-        PREFIX LoL: <http://www.semanticweb.org/drunkfazor/ontologies/2024/2/LoL#>
-        select ?label ?comment
-        where {
-            ?x rdf:type LoL:Modalità.
-            OPTIONAL { ?x rdfs:label ?label }
-            OPTIONAL { ?x rdfs:comment ?comment }
+        PREFIX LoL: <http://www.semanticweb.org/gfazor/ontologies/LOL#>
+        SELECT?label?comment
+        WHERE {
+          ?x rdf:type LoL:Modalità.
+            OPTIONAL {?x rdfs:label?label }
+            OPTIONAL {?x rdfs:comment?comment }
         }
-
         """
         return self.execute_query(query)
 
+    # Method to get tournaments participated by a specific player
     def get_tournaments_participated_by_player(self):
         query = """
         PREFIX wd: <http://www.wikidata.org/entity/>
         PREFIX wdt: <http://www.wikidata.org/prop/direct/>
         PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
         PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-
-        SELECT ?label ?time
+        SELECT?label?time
         WHERE {
             SERVICE <https://query.wikidata.org/sparql>{
                 OPTIONAL {
-                    wd:Q15618298 wdt:P1344 ?link
+                    wd:Q15618298 wdt:P1344?link
                 }
                 OPTIONAL {
-                    ?link rdfs:label ?label;
-                    wdt:P585 ?time.
+                  ?link rdfs:label?label;
+                    wdt:P585?time.
                 }
             }
             FILTER(LANG(?label) = "en")
         }
-        ORDERBY ?time
-
+        ORDER BY?time
         """
         return self.execute_query(query)
 
+    # Method to get teams played by a specific player
     def get_teams_played_by_player(self):
         query = """
         PREFIX wd: <http://www.wikidata.org/entity/>
         PREFIX wdt: <http://www.wikidata.org/prop/direct/>
         PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-        select  ?label  ?comment
+        SELECT?label?comment
         WHERE {
             SERVICE <https://query.wikidata.org/sparql>{
             OPTIONAL{
-                wd:Q18608567 wdt:P54 ?team
+                wd:Q18608567 wdt:P54?team
             }
-            OPTIONAL { ?team rdfs:label ?label }
-            OPTIONAL { ?team rdfs:comment ?comment }
+            OPTIONAL {?team rdfs:label?label }
+            OPTIONAL {?team rdfs:comment?comment }
                 FILTER(LANG(?label) = "en")
-
             }
         }
-
         """
         return self.execute_query(query)
